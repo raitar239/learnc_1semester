a0c2b398ef87b36490ad49868c02907af2c01079
Raitar <raitar239@gmail.com>
2025-11-24 10:47:34 +0700

    Добавлена функция main

--- lab9/sort.c
+++ lab9/sort.c
@@ -1,4 +1,6 @@
 // Quick sort 
+#include <stdio.h>
+
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
         int pivot = arr[high];
@@ -22,4 +24,25 @@ void quickSort(int arr[], int low, int high) {
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
+}
+
+int main() {
+    int a[] = {4, 0, 7, 1, 9, 3};
+    int n = sizeof(a) / sizeof(a[0]);
+    
+    printf("Исходный массив: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    
+    quickSort(a, 0, n - 1);
+    
+    printf("Отсортированный массив: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    
+    return 0;
 }
